0e813c2cc25b7c9b0eeaf9e162822428969b2974
vagdom <vagdom@hotmail.com>
2017-12-04 18:51:20 -0500

    Create newString.cpp

---
+++ newString.cpp
@@ -0,0 +1,10 @@
+//Implementation file newString.cpp
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cassert>
+#include "newString.h"
+
+using namespace std;
+
+//Constructor; conversion from the char string to newString
