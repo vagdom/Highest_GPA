49de24417b8d4aece30e28b92e434528ee32e34d
vagdom <vagdom@hotmail.com>
2017-12-04 18:24:26 -0500

    Update newString.h

--- newString.h
+++ newString.h
@@ -8,10 +8,36 @@ using namespace std;
 class newString
 {
 
+  //Overload the stream insertion and extraction operator
+  friend ostream& operator<<(ostream&, const newString&);
+  friend istream& operator>>(istream&, newString&);
+  
+public:
+  //Overload the assign operator
+  const newString& operator=(const newString&);
+  //Constructor; conversion from the char string
+  newString(const char *);
+  //Default constructor to initialize the string to NULL
+  newString();
+  //Copy constructor
+  newString(const newString&);
+  //Desctructor
+  ~newString();
 
-
-
-
-
+  char& operator[] (int);
+  const char& operator[] (int) const;
+  
+  //Overload the relational operators
+  bool operator==(const newString&) const;
+  bool operator!=(const newString&) const;
+  bool operator<=(const newString&) const;
+  bool operator<(const newString&) const;
+  bool operator>=(const newString&) const;
+  bool operator>(const newString&) const;
+ private:
+  char *strPtr;                   //Pointer to the char array
+                                  //that holds the string
+  int strLength;                  //data member to store the length
+                                  //of the string
 };
 #endif
