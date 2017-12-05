4f0d3cf8b898c31f250f4493615e9e540e843cab
vagdom <vagdom@hotmail.com>
2017-12-04 21:28:44 -0500

    Update newString.cpp

--- newString.cpp
+++ newString.cpp
@@ -35,3 +35,82 @@ newString::newString(const newString& rightStr)
   strcpy(strPtr, rightStr.strPtr);
 }
 
+//Destructor
+newString::~newString()
+{
+  delete [] strPtr;
+}
+
+//Overload the assignment operator
+const newString& newString::operator=(const newString& rightString)
+{
+  if(this != &rightStr)              //avoid self-copy
+  {
+    delete [] = strPtr;
+    strLength = right.strLength;
+    strPtr = new char[strLength + 1];
+    assert(strPtr != NULL);
+    strcpy(strPtr, right.strPtr);
+  }
+  return *this;
+}
+
+char& newString::operator[] (int index)
+{
+  assert(0 <= index && index < strLength);
+  return strPtr[index];
+}
+
+const char& newString::operator[] (int index) const
+{
+  assert(0 <= index && index < strLength);
+  return strPtr[index];
+}
+
+//Overload the relational operators
+bool newString::operator==(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) == 0);
+}
+
+bool newString::operator<(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) < 0);
+}
+
+bool newString::operator<=(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) <= 0);
+}
+
+bool newString::operator>(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) > 0);
+}
+
+bool newString::operator>=(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) >= 0);
+}
+
+bool newString::operator!=(const newString& rightStr) const
+{
+  return (strcmp(strPtr, rightStr.strPtr) != 0);
+}
+
+//Overload the stream insertion operator <<
+ostream& operator<<(ostream& object, const newString& str)
+{
+  osObject << str.strPtr;
+  return osObject;
+}
+
+//Overload the stream extraction operator >>
+istream& operator>>(istream& isObject, newString& str)
+{
+  char temp[81];
+  
+  isObject>>setw(81)>>temp;
+  str = temp;
+  return isObject;
+}
