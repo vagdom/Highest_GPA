0ec5c37065701202f4adf7144ae1091cbe6ebfff
vagdom <vagdom@hotmail.com>
2017-12-04 19:44:30 -0500

    Update newString.cpp

--- newString.cpp
+++ newString.cpp
@@ -8,3 +8,30 @@
 using namespace std;
 
 //Constructor; conversion from the char string to newString
+newString::newString(const char *str);
+{
+  strLength = strlen(str);
+  strPtr = new char[strlength +1];                 //Allocate memory to store
+                                                   //the char string
+  assert(strPtr != NULL);
+  strcpy(strPtr, str);                             //Copy the string into strPtr
+}
+
+//Default constructor to store the null string
+newString::newString()
+{
+  strLength = 0;
+  strPtr = new char[1];
+  assert(strPtr != NULL);
+  strcpy(strPtr, "");
+}
+
+//Copy constructor
+newString::newString(const newString& rightStr)
+{
+  strLength = rightStr.strLength;
+  strPtr = new char[strLength + 1];
+  assert( strPtr != NULL);
+  strcpy(strPtr, rightStr.strPtr);
+}
+
